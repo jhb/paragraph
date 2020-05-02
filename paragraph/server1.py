e0d096655d85d1d261cf3cded077163e5385fa13
Joerg Baach <mail@baach.de>
2020-05-02 11:29:48 +0200

    Starting to work on server structure

---
+++ paragraph/server1.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from paragraph.tal_templates import TalTemplates
+app = Flask(__name__)
+
+templates = TalTemplates()
+
+@app.route('/')
+def hello_world():
+    return templates.main(foo=0)
+
+if __name__ == '__main__':
+    app.run(debug=True)
