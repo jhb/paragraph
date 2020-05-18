c3743218b324bfaeba0074a3dce5a059e3f02ab7
Joerg Baach <mail@baach.de>
2020-05-18 15:13:10 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='paragraph',
+      version='0.0.1',
+      description='UI to graph databases',
+      url='',
+      author='GraphUI devs',
+      author_email='devs@graphui.org',
+      license='GPLV3',
+      packages=['paragraph'],
+      zip_safe=False)
